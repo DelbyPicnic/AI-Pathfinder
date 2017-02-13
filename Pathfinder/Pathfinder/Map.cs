65fa0a025c1db87523f18d13af393ace95bdd003
Gordon Swan <40202556@live.napier.ac.uk>
2017-02-13 17:27:28 +0000

    Application structure in place, further expansion of the Node class required.

---
+++ Pathfinder/Pathfinder/Map.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinder
+{
+    class Map
+    {
+        //Define class vars
+        private int numNodes = 0;       //The number of distinct nodes on the map
+        private int[,] coordNodes;      //The coordinates of each node
+
+        //Class to read all data from file and represend the data
+            //Open file and read data into a data buffer
+            //Separate data according to the provided file structure
+            //Store appropriate data in correct format (ie int for number of nodes
+
+        //Method to return the nuber of nodes
+        //Method to return the coordinate for a node
+        //Method to determine the validity of a move based on two nodes.
+        //Method to return the relationship data for a perticular node.
+    }
+}
---
+++ Pathfinder/Pathfinder/Node.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinder
+{
+    class Node
+    {
+        private int nodeValue;                              //Store the indexed value for this node
+        private int posX, posY;                             //Store the X & Y coordinates for this node
+        private List<int> pathRelations = new List<int>();  //Store the navigation relationship between this node and every other node
+
+
+        public int NodeValue                                //Modifier method for setting and retrieving the NodeValue
+        {
+            get { return nodeValue; }
+            set { nodeValue = value; }
+        }
+
+        public int PosX                                     //Modifier method for setting and retrieving the X Coordinate
+        {
+            get { return posX; }
+            set { posX = value; }
+        }
+        public int PosY                                     //Modifier method for setting and retrieving the Y Coordinate
+        {
+            get { return posY; }
+            set { posY = value; }
+        }
+
+        public List<int> PathRelations                      //Modifier method for setting and retrieving the navigation relationship list
+        {
+            get { return pathRelations; }
+            set { pathRelations = value; }
+        }
+
+        public bool PathToNode(int A, int B)                //Method to determine if an AI can pass from node A to node B (A -> B != B -> A) 
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
