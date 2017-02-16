07d5550d6239a2f877c474ecfff4ef6764ed09bf
Gordon Swan <40202556@live.napier.ac.uk>
2017-02-16 16:42:58 +0000

    Coordinate load function for each node is incorrect and needs fixed, current implementation actually causes a stack underflow error, will fix in 10 mins

--- Pathfinder/Pathfinder/Map.cs
+++ Pathfinder/Pathfinder/Map.cs
@@ -39,38 +39,43 @@ namespace Pathfinder
                         //All values are present
                         MessageBox.Show("Number of Nodes: " + fValues[0] + " All required values are present. T: " + Convert.ToString(fValues.Length));  //FOR DEBUG ONLY; REMOVE LATER.
 
-                        //Create node instances for each node
-                        for (int index = 0; index < Convert.ToInt32(fValues[0]); index++)
-                        {
-                            Node newNode = new Node();                              //Define a new node to be added to the list
-                            newNode.NodeValue = index;                              //Set the new node's index value (Not actually required but whatever)
-                            newNode.PosX = Convert.ToInt32(fValues[index + 1]);     //Set the new node's X Coordinate
-                            newNode.PosY = Convert.ToInt32(fValues[index + 2]);     //Set the new node's Y Coordinate
+                            //Create node instances for each node
+                            for (int index = 0; index < Convert.ToInt32(fValues[0]); index++)
+                            {
+                                Node newNode = new Node();                              //Define a new node to be added to the list
+                                newNode.NodeValue = index;                              //Set the new node's index value (Not actually required but whatever)
+                                newNode.PosX = Convert.ToInt32(fValues[(index*2)-1]);     //Set the new node's X Coordinate
+                                newNode.PosY = Convert.ToInt32(fValues[index*2]);     //Set the new node's Y Coordinate
 
-                            //Create a list for the new node's path relationship data
-                            List<int> lstPaths = new List<int>();
-                            try
-                            {
-                                for (int i = 0; i < Convert.ToInt32(fValues[0]); i++)
+                                //Create a list for the new node's path relationship data
+                                List<int> lstPaths = new List<int>();
+                                try
                                 {
-                                    int curValue = Convert.ToInt32(fValues[(Convert.ToInt32(fValues[0]) * 2) + (Convert.ToInt32(fValues[0]) * index) + 1 + i]);
-                                    if (curValue > 1 || curValue < 0)
+                                    for (int i = 0; i < Convert.ToInt32(fValues[0]); i++)
                                     {
-                                        throw new Exception("Path relationship value must be either 0 or 1. Check file and reload");
+                                        int curValue = Convert.ToInt32(fValues[(Convert.ToInt32(fValues[0]) * 2) + (Convert.ToInt32(fValues[0]) * index) + 1 + i]);
+                                        if (curValue > 1 || curValue < 0)
+                                        {
+                                            throw new Exception("Path relationship value must be either 0 or 1. Check file and reload");
+                                        }
+                                        else
+                                        {
+                                            lstPaths.Add(curValue); //If something goes wrong it's probably this...
+                                        }
                                     }
-                                    else
-                                    {
-                                        lstPaths.Add(curValue); //If something goes wrong it's probably this...
-                                    }
+                                    newNode.PathRelations = lstPaths;
                                 }
-                                newNode.PathRelations = lstPaths;
+                                catch (Exception e)
+                                {
+                                    MessageBox.Show("Something's not right: " + e.Message);
+                                }
+                                mapNodes.Add(newNode);
                             }
-                            catch(Exception e)
+
+                            for (int i = 0; i < mapNodes.Count; i++)
                             {
-                                MessageBox.Show("Something's not right: " + e.Message);
+                                MessageBox.Show("COORDINATES: " + mapNodes[i].PosX + "," + mapNodes[i].PosY);
                             }
-                            mapNodes.Add(newNode);
-                        }
                     }
                     
                 }
